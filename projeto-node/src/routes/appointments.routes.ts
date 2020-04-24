4529a5658fbbc8041a985db882a6d1c9641b3cac
Asonac <wesley.canosa@gmail.com>
2020-04-24 08:57:55 -0300

    done

--- projeto-node/src/routes/appointments.routes.ts
+++ projeto-node/src/routes/appointments.routes.ts
@@ -1,40 +1,43 @@
 import { Router } from 'express';
 import { parseISO } from 'date-fns';
+import { getCustomRepository } from 'typeorm';
+
 import AppointmentsRepository from '../repository/AppointmentsRepository';
 import CreateAppointmentService from '../services/CreateAppointmentService';
+import ensureAuthenticated from '../middleware/ensureAuthenticated';
 
 const appointmentsRouter = Router();
-const appointmentsRepository = new AppointmentsRepository();
+
+appointmentsRouter.use(ensureAuthenticated);
 
 // DTO - Data Transfer Object
 
 // Rota: Receber a requisição, chamar outro arquivo, devolver uma resposta
 
-appointmentsRouter.get('/', (request, response) => {
-  const appointments = appointmentsRepository.all();
+appointmentsRouter.get('/', async (request, response) => {
+  const appointmentsRepository = getCustomRepository(AppointmentsRepository);
+  const appointments = await appointmentsRepository.find();
 
   return response.json(appointments);
 });
 
-appointmentsRouter.post('/', (request, response) => {
-  try {
-    const { provider, date } = request.body;
+appointmentsRouter.post('/', async (request, response) => {
+  // try {
+  const { provider_id, date } = request.body;
 
-    const parsedDate = parseISO(date);
+  const parsedDate = parseISO(date);
 
-    const createAppointment = new CreateAppointmentService(
-      appointmentsRepository,
-    );
+  const createAppointment = new CreateAppointmentService();
 
-    const appointment = createAppointment.execute({
-      date: parsedDate,
-      provider,
-    });
+  const appointment = await createAppointment.execute({
+    date: parsedDate,
+    provider_id,
+  });
 
-    return response.json(appointment);
-  } catch (err) {
-    return response.status(400).json({ error: err.message });
-  }
+  return response.json(appointment);
+  // } catch (err) {
+  //   return response.status(400).json({ error: err.message });
+  // }
 });
 
 export default appointmentsRouter;
